22babc655295bf7fc57af32d32fae0b1a1feac1c
RyanGuinchard <ryanguinchard9@hotmail.com>
2024-06-09 16:22:58 -0230

    Started backend before branching.

--- server/index.js
+++ server/index.js
@@ -1,11 +1,20 @@
 /* eslint-env node */
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const { MongoClient } = require('mongodb');
+const express = require("express");
+const { MongoClient } = require("mongodb");
 
 const app = express();
 
 //Middleware to parse JSON
 app.use(express.json());
 
+// TODO: Add route files
+
+// TODO: Mount route files
+
+// Start server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});

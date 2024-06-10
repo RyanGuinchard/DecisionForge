c144ffcce471bba2518e66df6a25cb1ed7df30e9
RyanGuinchard <ryanguinchard9@hotmail.com>
2024-06-10 17:47:20 -0230

    chore: Add Tailwind CSS configuration files and update dependencies

---
+++ src/pages/Mainpage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Mainpage = () => {
+  return (
+    <>
+      <div>
+        <h1>Decision Forge</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum dolorum sapiente nostrum at saepe nam a aperiam voluptatem est obcaecati? Temporibus distinctio nam excepturi recusandae porro cum sapiente rem rerum?</p>
+      </div>
+    </>
+  );
+};
+
+export default Mainpage;

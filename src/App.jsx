fb9d9dff9c492ef8dff5ae99a710fd69cd8129d3
RyanGuinchard <ryanguinchard9@hotmail.com>
2024-06-10 18:48:53 -0230

    chore: Update CSS and component structure for sidebar navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Mainpage from './pages/Mainpage'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Mainpage from "./pages/main/Mainpage";
+import Game from "./pages/game/Game";
+import Sidebar from "./components/sidebar/Sidebar";
+import "./App.css";
 
 function App() {
-
   return (
     <>
       <Router>
-        <Routes>
-          <Route path="/" element={<Mainpage />} />
-        </Routes>
+        <div className="flex">
+          <Sidebar />
+          <div>
+            <Routes>
+              <Route path="/" element={<Mainpage />} />
+              <Route path="/game/:title" element={<Game />} />
+            </Routes>
+          </div>
+        </div>
       </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
